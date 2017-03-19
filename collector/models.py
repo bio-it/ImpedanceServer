95e35be9309ed16ec1487443dab7a48e93533af7
mauver <mauver@naver.com>
2017-03-19 23:40:36 +0900

    Temp saving..

--- collector/models.py
+++ collector/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Model for dwf 
-   
+
 class DwfResultData(models.Model):
 	dataCounter = models.IntegerField(primary_key=True, default=0)
 	startTime = models.DateTimeField(blank=True, null=True)
@@ -22,3 +22,18 @@ class DwfMeasureData(models.Model):
 	channel = models.IntegerField(null=False, default='')
 	time = models.DateTimeField(blank=True, null=True)
 	timeMin = models.IntegerField(null=False, default=0)
+
+class Parameter(models.Model):
+	command = models.TextField(null=False, default="")
+	state = models.TextField(null=False, default="")
+	channel = models.TextField(null=False, default="")
+	freq = models.TextField(null=False, default="")
+	chipinfo = models.TextField(null=False, default="")
+	deadline = models.TextField(null=False, default="")
+	error = models.TextField(null=False, default="")
+	result = models.TextField(null=False, default="")
+	period = models.TextField(null=False, default="")
+	record_state = models.TextField(null=False, default="")
+	counter = models.TextField(null=False, default="")
+	elapsed_time = models.TextField(null=False, default="")
+	start_time = models.TextField(null=False, default="")
